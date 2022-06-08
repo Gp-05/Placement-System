1e70e95c0fc1989b14809eea01406124981c0759
DELL <DELL@PritiRaje>
2022-06-08 16:16:51 +0530

    commit with priti

--- Placement-System/src/main/java/com/placement/demo/PlacementSystemApplication.java
+++ Placement-System/src/main/java/com/placement/demo/PlacementSystemApplication.java
@@ -8,6 +8,8 @@ public class PlacementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlacementSystemApplication.class, args);
+	
+	System.out.println("hello priti");
 	}
 
 }
