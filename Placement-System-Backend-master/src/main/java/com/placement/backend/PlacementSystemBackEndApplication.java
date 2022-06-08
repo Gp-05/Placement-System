c98fcb5ac36890ada7bbe87c116b44dd741502ee
indianic <indianic@ind992>
2022-06-08 14:49:03 +0530

    First Commit

---
+++ Placement-System-Backend-master/src/main/java/com/placement/backend/PlacementSystemBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.placement.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementSystemBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementSystemBackEndApplication.class, args);
+	}
+
+}
